83262ee464a9eb649e401037fb0e79f192317a51
Aaron McKie <aaron.mckie@icloud.com>
2021-04-15 15:52:01 +0100

    MVP completed, some extensions complete

--- day_4/02_flask_template_lab/flask_lab/controllers/controller.py
+++ day_4/02_flask_template_lab/flask_lab/controllers/controller.py
@@ -1,21 +1,21 @@
 from flask import render_template, request
 from app import app
+from models.events_example import events, create_event
 from models.event import Event
-from models.events_example import events
+
 
 @app.route("/")
 def index():
     return render_template('index.html', title="Home", events=events)
 
 @app.route('/', methods=['POST'])
-def add_task():
+def add_event():
     date = request.form['date']
     name_of_event = request.form['name_of_event']
     number_of_guests = request.form['number_of_guests']
     room_location = request.form['room_location']
     description = request.form['description']
-    new_event = Event(date, name_of_event, number_of_guests, room_location, description)
-    print(request.form)
+    new_event = Event(date, name_of_event, number_of_guests, room_location, description, False)
     create_event(new_event)
     return render_template('index.html', title="Home", events=events)
     
--- day_4/02_flask_template_lab/flask_lab/models/events_example.py
+++ day_4/02_flask_template_lab/flask_lab/models/events_example.py
@@ -1,8 +1,8 @@
 from models.event import *
 
-event1 = Event("10/07/2021", "Metallica", 1000, "Belhouston", "Heavy Metal")
+event1 = Event("10/07/2021", "Metallica", 1000, "Belhouston", "Heavy Metal", True)
 
-event2 = Event("06/08/2021", "Chic", 500, "Kelvingrove Band Stand", "Disco")
+event2 = Event("06/08/2021", "Chic", 500, "Kelvingrove Band Stand", "Disco", False)
 
 events = [event1, event2]
 
