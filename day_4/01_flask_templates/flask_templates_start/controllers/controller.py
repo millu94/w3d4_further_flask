ef487c358c01e3473c81f6cec4727bdd3aeb8311
Aaron McKie <aaron.mckie@icloud.com>
2021-04-15 13:32:05 +0100

    Codealong lesson completed

--- day_4/01_flask_templates/flask_templates_start/controllers/controller.py
+++ day_4/01_flask_templates/flask_templates_start/controllers/controller.py
@@ -1,7 +1,17 @@
-from flask import render_template # ADDED
+from flask import render_template, request
 from app import app
-from models.todo_list import tasks
+from models.todo_list import tasks, add_new_task
+from models.task import Task
 
 @app.route('/tasks')
 def index():
     return render_template('index.html', title='Home', tasks=tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    task_title = request.form['title']
+    task_desc = request.form['description']
+    new_task = Task(task_title, task_desc, False)
+    print(request.form)
+    add_new_task(new_task)
+    return render_template('index.html', title="Home", tasks=tasks)
